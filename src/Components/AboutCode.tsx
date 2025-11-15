67241ae37649ba73672e79186e83b7b3b1ad0510
linnclaesson <linn.denise.claesson@gmail.com>
2025-11-15 13:51:57 +0100

    about page with code block component

---
+++ src/Components/AboutCode.tsx
@@ -0,0 +1,144 @@
+export const AboutCode = () => {
+  return (
+    <section className="font-roboto-mono bg-teal-950 p-5 text-white">
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">1</span>
+        <span>
+          <span className="text-indigo-400">const</span>{" "}
+          <span className="text-rose-500">aboutMe</span> = {"{"}
+        </span>
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">2</span>
+        <span className="pl-4 text-amber-300">info</span>: {"{"}
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">3</span>
+        <span className="pl-8 text-amber-600">name</span>:
+        <span className="pl-3 text-teal-500">"Linn Claesson"</span>,
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">4</span>
+        <span className="pl-8 text-amber-600">email</span>:
+        <span className="pl-3 text-teal-500">"claesson.dev@gmail.com"</span>,
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">5</span>
+        <span className="pl-8 text-amber-600">location</span>:
+        <span className="pl-3 text-teal-500">"Lund, Sweden"</span>,
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">6</span>
+        <span className="pl-4">{"},"}</span>
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">7</span>
+        <span className="pl-4 text-amber-300">experience</span>: [
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">8</span>
+        <span className="pl-8">{"{"}</span>
+        <span className="pl-3 text-amber-600">time</span>:
+        <span className="pl-3 text-teal-500">"nov 2025 - may 2026"</span>,
+        <span className="pl-3 text-amber-600">role</span>:
+        <span className="pl-3 text-teal-500">"frontend developer"</span>,
+        <span className="pl-3 text-amber-600">organisation</span>:
+        <span className="pl-3 text-teal-500">"Lundakarnevalen"</span>
+        <span className="pl-3">{"}"}</span>,
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">9</span>
+        <span className="pl-8">{"{"}</span>
+        <span className="pl-3 text-amber-600">time</span>:
+        <span className="pl-3 text-teal-500">"jan 2025 - apr 2025"</span>,
+        <span className="pl-3 text-amber-600">role</span>:
+        <span className="pl-3 text-teal-500">"fullstack developer intern"</span>
+        ,<span className="pl-3 text-amber-600">organisation</span>:
+        <span className="pl-3 text-teal-500">"Devoteam"</span>
+        <span className="pl-3">{"}"}</span>,
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">10</span>
+        <span className="pl-8">{"{"}</span>
+        <span className="pl-3 text-amber-600">time</span>:
+        <span className="pl-3 text-teal-500">"may 2024 - jun 2024"</span>,
+        <span className="pl-3 text-amber-600">role</span>:
+        <span className="pl-3 text-teal-500">"frontend developer intern"</span>,
+        <span className="pl-3 text-amber-600">organisation</span>:
+        <span className="pl-3 text-teal-500">"Comunica"</span>
+        <span className="pl-3">{"}"}</span>,
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">11</span>
+        <span className="pl-4">],</span>
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">12</span>
+        <span className="pl-4 text-amber-300">education</span>: [
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">13</span>
+        <span className="pl-8">{"{"}</span>
+        <span className="pl-3 text-amber-600">time</span>:
+        <span className="pl-3 text-teal-500">"2023 - 2025"</span>,
+        <span className="pl-3 text-amber-600">program</span>:
+        <span className="pl-3 text-teal-500">
+          "Fullstack developer - javascript"
+        </span>
+        ,<span className="pl-3 text-amber-600">school</span>:
+        <span className="pl-3 text-teal-500">"Teknikhögskolan"</span>
+        <span className="pl-3">{"}"}</span>,
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">14</span>
+        <span className="pl-8">{"{"}</span>
+        <span className="pl-3 text-amber-600">time</span>:
+        <span className="pl-3 text-teal-500">"2020 - 2023"</span>,
+        <span className="pl-3 text-amber-600">program</span>:
+        <span className="pl-3 text-teal-500">"Iinteraction design"</span>,
+        <span className="pl-3 text-amber-600">school</span>:
+        <span className="pl-3 text-teal-500">"Malmö Univeristy"</span>
+        <span className="pl-3">{"}"}</span>,
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">15</span>
+        <span className="pl-4">],</span>
+      </div>
+
+      <div className="flex flex-wrap">
+        <span className="w-8 pr-3 text-right text-teal-900">16</span>
+        <span className="pl-4 text-amber-300">skills</span>: [
+        <span className="pl-3 text-teal-500">"HTML/CSS/JS"</span>,
+        <span className="pl-3 text-teal-500">"TypeScript"</span>,
+        <span className="pl-3 text-teal-500">"React"</span>,
+        <span className="pl-3 text-teal-500">"Node.js"</span>,
+        <span className="pl-3 text-teal-500">"Java"</span>,
+        <span className="pl-3 text-teal-500">"Spring Boot"</span>,
+        <span className="pl-3 text-teal-500">"Docker"</span>,
+        <span className="pl-3 text-teal-500">"PostgreSQL"</span>,
+        <span className="pl-3 text-teal-500">"MongoDB"</span>,
+        <span className="pl-3 text-teal-500">"Tailwind"</span>
+        <span className="pl-4">],</span>
+      </div>
+
+      <div className="flex">
+        <span className="w-8 pr-3 text-right text-teal-900">17</span>
+        <span>{"};"}</span>
+      </div>
+    </section>
+  );
+};
---
+++ src/Pages/AboutPage.tsx
@@ -0,0 +1,12 @@
+import { AboutCode } from "../Components/AboutCode";
+
+export const AboutPage = () => {
+  return (
+    <>
+      <section>
+        <h1>This is my about page</h1>
+      </section>
+      <AboutCode />
+    </>
+  );
+};
